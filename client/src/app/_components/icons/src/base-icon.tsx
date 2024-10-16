be5fd0463b710d1eb568c991e241c572712a7f99
Afshin Ghezeli <a.g.afshin@gmail.com>
2024-10-16 11:00:00 +0330

    feat: add BaseIcon component

---
+++ client/src/app/_components/icons/src/base-icon.tsx
@@ -0,0 +1,31 @@
+import { SVGAttributes } from "react";
+
+const BaseIcon: React.FC<SVGAttributes<SVGElement>> = ({
+  color = "primary",
+  fill = "none",
+  width = 24,
+  height = 24,
+  strokeWidth = "1.5",
+  viewBox = "0 0 24 24",
+  children,
+  ...res
+}) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+      strokeWidth={strokeWidth}
+      viewBox={viewBox}
+      fill={fill}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      stroke={color}
+      {...res}
+    >
+      {children}
+    </svg>
+  );
+};
+
+export default BaseIcon;
