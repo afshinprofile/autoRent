53131636ad1e3f186a5b0bc17bfc0560d549a2e3
Afshin Ghezeli <a.g.afshin@gmail.com>
2024-10-16 11:00:00 +0330

    feat: convert all svg icons to component

---
+++ client/icons/gulpfile.js
@@ -0,0 +1,85 @@
+const gulp = require("gulp"),
+    replace = require("gulp-replace"),
+    rename = require("gulp-rename"),
+    svgmin = require("gulp-svgmin"),
+    wrap = require("gulp-wrap"),
+    fs = require("fs");
+
+const svg_files_folder = "./icons-svg",
+    dest_folder = "../src/app/_components/icons/",
+    jsx_template = "./icon-template.txt";
+
+let icons_component_list = [];
+
+gulp.task("icons_component", () => {
+    return gulp
+        .src([svg_files_folder + "/*.svg"])
+        .pipe(
+            svgmin({
+                multipass: true,
+                plugins: [
+                    // پاک کردن ID‌های غیرضروری
+                    { name: "cleanupIDs", params: { remove: true, minify: true } },
+                    // حذف داکیوتایپ
+                    { name: "removeDoctype" },
+                    // حذف کامنت‌ها
+                    { name: "removeComments" },
+                    // حذف عنوان
+                    { name: "removeTitle" },
+                    // حذف ابعاد
+                    { name: "removeDimensions" },
+                    // فشرده‌سازی گروه‌ها
+                    { name: "collapseGroups" },
+                    // تبدیل رنگ‌ها به hex
+                    { name: "convertColors", params: { names2hex: true, rgb2hex: true } },
+                    // حذف عناصر style
+                    { name: "removeStyleElement" },
+                    // حذف کانتینر‌های خالی
+                    { name: "removeEmptyContainers" },
+                    // حذف ویژگی‌های غیرضروری
+                    {
+                        name: "removeAttrs",
+                        params: {
+                            attrs: [
+                                "(filter|fill|stroke|fill-rule|stroke-linecap|stroke-linejoin|stroke-width|transform|style|class|data.*)",
+                                "svg:(width|height)",
+                            ],
+                        },
+                    },
+                    // حذف عناصر useless defs
+                    { name: "removeUselessDefs" },
+                ],
+            })
+        )
+        .pipe(replace(/<\/?svg(.*?)>/g, "")) // حذف تگ svg
+        .pipe(wrap({ src: jsx_template })) // پیش‌قالب JSX
+        .pipe(rename(function (path) {
+            icons_component_list.push(path.basename); // ثبت نام فایل‌ها
+            path.extname = ".tsx"; // تغییر پسوند به .tsx
+        }))
+        .pipe(gulp.dest(dest_folder + "/src")); // ذخیره در مقصد
+});
+
+gulp.task("icons_component_main", (done) => {
+    const content = "/* GENERATED FILE */\n\n" +
+        icons_component_list
+            .map((item) => {
+                let module_name = (
+                    item.charAt(0).toUpperCase() + item.substr(1)
+                ).replace(/-([a-z])/g, (g) => g[1].toUpperCase());
+                return `export {default as Icon${module_name}} from './src/${item}';`;
+            })
+            .join("\n");
+
+    // استفاده از fs.writeFile استاندارد
+    fs.writeFile(dest_folder + "/index.ts", content, (err) => {
+        if (err) {
+            console.error("Error writing file:", err);
+            return done(err); // اعلام خطای async
+        }
+        console.log("File written successfully.");
+        done(); // نشان دادن پایان TasK
+    });
+});
+
+gulp.task("default", gulp.series("icons_component", "icons_component_main"));
---
+++ client/src/app/_components/icons/index.ts
@@ -0,0 +1,23 @@
+/* GENERATED FILE */
+
+export {default as IconAddSquare} from './src/addSquare';
+export {default as IconArrowCircleLeft} from './src/arrowCircleLeft';
+export {default as IconArrowDown} from './src/arrowDown';
+export {default as IconArrowLeft} from './src/arrowLeft';
+export {default as IconBox} from './src/box';
+export {default as IconCalendar} from './src/calendar';
+export {default as IconCalendarTick} from './src/calendarTick';
+export {default as IconCallCalling} from './src/callCalling';
+export {default as IconCar} from './src/car';
+export {default as IconCards} from './src/cards';
+export {default as IconClock} from './src/clock';
+export {default as IconCreativeCommons} from './src/creativeCommons';
+export {default as IconEye} from './src/eye';
+export {default as IconKey} from './src/key';
+export {default as IconMap} from './src/map';
+export {default as IconMinusSquare} from './src/minusSquare';
+export {default as IconSearchNormal} from './src/searchNormal';
+export {default as IconSms} from './src/sms';
+export {default as IconStar} from './src/star';
+export {default as IconSupport24} from './src/support24';
+export {default as IconWalletMinus} from './src/walletMinus';
