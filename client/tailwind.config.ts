ea83b975e631990038eba622b0ca68c465c09e0b
Afshin Ghezeli <a.g.afshin@gmail.com>
2024-10-18 11:00:00 +0330

    feat(tailwind): extend screen breakpoint

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -62,6 +62,12 @@ export default {
       fontFamily: {
         Estedad: ["var(--font-estedad)"],
       },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
     },
   },
   plugins: [],
